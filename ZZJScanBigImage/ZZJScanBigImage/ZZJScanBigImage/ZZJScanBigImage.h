ae801db5825c252ac5bb7892d606cc6dd755efce
mac <JOE@macdeMac-mini.local>
2017-05-17 14:05:49 +0800

    1.创建项目 2.新建ZZJScanBigImage类

---
+++ ZZJScanBigImage/ZZJScanBigImage/ZZJScanBigImage/ZZJScanBigImage.h
@@ -0,0 +1,13 @@
+//
+//  ZZJScanBigImage.h
+//  ZZJScanBigImage
+//
+//  Created by JOE on 2017/5/17.
+//  Copyright © 2017年 ZZJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZZJScanBigImage : NSObject
+
+@end
---
+++ ZZJScanBigImage/ZZJScanBigImage/ZZJScanBigImage/ZZJScanBigImage.m
@@ -0,0 +1,13 @@
+//
+//  ZZJScanBigImage.m
+//  ZZJScanBigImage
+//
+//  Created by JOE on 2017/5/17.
+//  Copyright © 2017年 ZZJ. All rights reserved.
+//
+
+#import "ZZJScanBigImage.h"
+
+@implementation ZZJScanBigImage
+
+@end
